699d7167e4163f4229e27309872ee429a2d6e33f
fivan14 <ivucetic7792@gmail.com>
2023-02-22 22:36:35 +0100

    Create operate and procedures functions, connected to events

--- script.js
+++ script.js
@@ -1,56 +1,130 @@
-const valueButtons = document.querySelector('#left-column')
+const digits = document.querySelectorAll('.digits')
 const display = document.querySelector('#display-result')
 const equal = document.querySelector('.equal')
 const operators = document.querySelector('#right-column')
 const clear = document.querySelector('.clear')
+const power = document.querySelector('#power-off')
+const audio = document.querySelector('audio')
+const screen = document.querySelector('#display')
 
 let num = 0;
 let operator = ''
 
 //defining sum substract, divide and product
+function sum(one, two) {
+    let final = one + two
+    display.textContent = final
+    num = final
+}
+
+function substract(one, two) {
+    let final = one - two
+    display.textContent = final
+    num = final
+}
+
+function division(one, two) {
+    let final = one / two
+    display.textContent = final
+    num = final
+}
+
+function multiply(one, two) {
+    let final = one * two
+    display.textContent = final
+    num = final
+}
+//functions for events
+
+function addFirstNumber(operatorValue) {
+    num = Number.parseInt(display.textContent, 10)
+    operator = operatorValue
+}
 
 
-//defining function operate()
+function displayResult(result) {
+    display.textContent = result
+    operator = ''
+}
 
+function clearResult() {
+    display.textContent = 0
+    num = 0
+    operator = ''
+}
 
+function powerOff() {
+    display.textContent = 0
+    num = 0
+    operator = ''
+    display.classList.toggle('hide')
+    screen.classList.toggle('powerOff')
+    screen.classList.toggle('powerOn')
 
-valueButtons.addEventListener('click', event => {
+    // audio play and disabling button for 0.5sec
+
+    audio.volume = 0.1
+    audio.play()
+    power.setAttribute('disabled', 'disabled')
+    setTimeout(() => {
+        power.removeAttribute('disabled')
+    }, 500)
     
-    console.log(event.target.classList.contains('digits'))
-    if (event.target.classList.contains('digits')) {
-        if (display.textContent === `${num}`) {
+}
+
+//*****************************************/
+
+digits.forEach(digit => {
+    
+    digit.addEventListener('click', event => {
+        console.log(`${num}`)
+        //let hideStatus = display.classList.contains('hide');
+        if (event.target.classList.contains('digits') && display.textContent === `${num}`) {
             display.textContent = ''
-    
         }
         display.textContent += event.target.textContent
-    }
-
+    })
 })
-
 
 operators.addEventListener('click', event => {
     
     if (!operator) {
-        num = Number.parseInt(display.textContent, 10)
-        console.log({num})
-        operator = event.target.textContent
+        addFirstNumber(event.target.textContent)
+        console.log(num, operator)
     } else {
-        let final = num + Number.parseInt(display.textContent, 10)
-        display.textContent = final
-        num = final
         operator = event.target.textContent
+        operate(num, Number.parseInt(display.textContent, 10), event.target.textContent)
     }
 })
 
 equal.addEventListener('click', event => {
-    let final = num + Number.parseInt(display.textContent)
-    display.textContent = final
-    num = final
+    operate(num, Number.parseInt(display.textContent, 10), operator)
     operator = ''
 })
 
-clear.addEventListener('click', () => {
-    display.textContent = 0
-    num = 0
-    operator = ''
-})
+clear.addEventListener('click', clearResult)
+
+
+power.addEventListener('click', powerOff)
+
+
+
+function operate(num1, num2, operator) {
+    switch (operator) {
+        case '+':
+            sum(num1, num2, operator)
+            break;
+        case '-': 
+            substract(num1, num2, operator)
+            break;
+        case '/':
+            division(num1, num2, operator)
+            break;
+        case '*': 
+            multiply(num1, num2, operator)
+            break;
+
+    }       
+}
+
+
