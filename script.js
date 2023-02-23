19715ae57edec537bf8337929c52842842595b13
fivan14 <ivucetic7792@gmail.com>
2023-02-23 18:37:31 +0100

    refine logic, fix division reminder to 3 decimals if float if result > display width display error

--- script.js
+++ script.js
@@ -10,29 +10,35 @@ const screen = document.querySelector('#display')
 let num = 0;
 let operator = ''
 
+
+
+// function is float
+
+function isFloat(value) {
+    if (typeof value === 'number' &&
+        !Number.isNaN(value) &&
+        !Number.isInteger(value)
+    ) {
+        return true;
+    }
+    return false;
+}
+
 //defining sum substract, divide and product
 function sum(one, two) {
-    let final = one + two
-    display.textContent = final
-    num = final
+    return one + two
 }
 
 function substract(one, two) {
-    let final = one - two
-    display.textContent = final
-    num = final
+    return one - two
 }
 
 function division(one, two) {
-    let final = one / two
-    display.textContent = final
-    num = final
+    return one / two
 }
 
 function multiply(one, two) {
-    let final = one * two
-    display.textContent = final
-    num = final
+    return  one * two
 }
 //functions for events
 
@@ -86,20 +92,53 @@ digits.forEach(digit => {
     })
 })
 
+/*
+when operator is clicked, check if there is already operator
+if (true) add number from display to num variable, 
+and add operator to operator variable
+*/
+
 operators.addEventListener('click', event => {
     
     if (!operator) {
         addFirstNumber(event.target.textContent)
         console.log(num, operator)
     } else {
-        operator = event.target.textContent
-        operate(num, Number.parseInt(display.textContent, 10), event.target.textContent)
+        operatorSign = event.target.textContent
+        let final = operate(num, Number.parseFloat(display.textContent), operatorSign)
+
+        // check if result fit the screen if not
+
+        if (`${final}`.length > 8 && isFloat(final)) {
+            display.textContent = final.toFixed(2)
+            num = final.toFixed(2)
+            console.log({num})
+            operator = event.target.textContent
+        } else {
+            display.textContent = final
+            num = final
+            operator = event.target.textContent
+        }
+        
     }
 })
 
 equal.addEventListener('click', event => {
-    operate(num, Number.parseInt(display.textContent, 10), operator)
-    operator = ''
+    let final = operate(num, Number.parseFloat(display.textContent), operator)
+    if (`${final}`.length > 8 && isFloat(final)) {
+        display.textContent = final.toFixed(2)
+        num = final.toFixed(2)
+    } else if (`${final}`.length > 8) {
+        display.textContent = 'error'
+        console.log(final)
+        console.log(final.toExponential())
+        num = final
+    } else {
+        display.textContent = final
+        console.log(final)
+        console.log(final.toExponential())
+        num = final
+    }
 })
 
 clear.addEventListener('click', clearResult)
@@ -112,17 +151,13 @@ power.addEventListener('click', powerOff)
 function operate(num1, num2, operator) {
     switch (operator) {
         case '+':
-            sum(num1, num2, operator)
-            break;
+            return sum(num1, num2, operator)
         case '-': 
-            substract(num1, num2, operator)
-            break;
+            return substract(num1, num2, operator)
         case '/':
-            division(num1, num2, operator)
-            break;
+            return division(num1, num2, operator)
         case '*': 
-            multiply(num1, num2, operator)
-            break;
+            return multiply(num1, num2, operator)
 
     }       
 }
