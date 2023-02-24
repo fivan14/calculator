90630af1fc56048272beb32c6043345b19be9df2
fivan14 <ivucetic7792@gmail.com>
2023-02-24 23:44:41 +0100

    worked on code estetics, needs work still xD

--- script.js
+++ script.js
@@ -92,15 +92,19 @@ function powerOff() {
 digits.forEach(digit => {
 
     digit.addEventListener('click', event => {
-        if (display.textContent.length === 8 && display.textContent != `${num}`) {
+        if (operators.classList.contains('active')) {
+            operators.classList.remove('active')
+        }
+        if (display.textContent.length === 8  && display.textContent != `${num}`) {
             return true
         } else {
+            //removes pulsing effect on numbers other than zero!
             if (display.classList.contains('pulse')) {
                 display.classList.toggle('pulse')
             }
             console.log(`${num}`)
-            //let hideStatus = display.classList.contains('hide');
-            if (event.target.classList.contains('digits') && display.textContent === `${num}`) {
+
+            if (display.textContent === `${num}`) {
                 display.textContent = ''
             }
             display.textContent += event.target.textContent
@@ -117,24 +121,38 @@ and add operator to operator variable
 operators.addEventListener('click', event => {
 
     if (event.target.classList.contains('operations')) {
-        if (!operator) {
-            addFirstNumber(event.target.textContent)
-            console.log(num, operator)
-        } else {
-            let operatorSign = event.target.textContent
-            let final = operate(num, Number.parseFloat(display.textContent), operatorSign)
-            if (!`${final}`.includes('e')) {
-                addAndDisplayNumber(final)
-                operator = operatorSign
+
+        // remembers the first operator that was clicked, if more are clicked in succession
+        if(!operators.classList.contains('active')) {
+        //===============================================
+
+            if (!operator) {
+                addFirstNumber(event.target.textContent)
+                console.log(num, operator)
+                operators.classList.add('active')
+            } else {
+                let operatorSign = event.target.textContent
+                let final = operate(num, Number.parseFloat(display.textContent), operatorSign)
+                if (!`${final}`.includes('e')) {
+                    addAndDisplayNumber(final)
+                    operator = operatorSign
+                    console.log(final)
+                }
             }
         }
     }
 })
 
 equal.addEventListener('click', event => {
-    let final = operate(num, Number.parseFloat(display.textContent), operator)
-    addAndDisplayNumber(final)
-    operator = ''
+
+    //prevents clicking equal immediately(in order not time wise) after operator button
+    if (!operators.classList.contains('active')) {
+        if (num) {
+            let final = operate(num, Number.parseFloat(display.textContent), operator)
+            addAndDisplayNumber(final)
+            operator = ''
+        }
+    }
 })
 
 clear.addEventListener('click', clearResult)
@@ -163,6 +181,11 @@ function operate(num1, num2, operator) {
 
 
 function removeLastChar() {
+    if(operators.classList.contains('active')) {
+        num = 0
+        operator = ''
+        operators.classList.remove('active')
+    }
     if (display.textContent === '0') {
         return true
     }
@@ -188,9 +211,12 @@ function addDecimalPoint() {
 
 function addAndDisplayNumber(final) {
     if (`${final}`.length > 8 && isFloat(final)) {
-        display.textContent = final.toFixed(2)
-        num = final.toFixed(2)
-    } else if (`${final}`.length > 8) {
+        let str = `${final.toExponential()}`.split('e')
+        display.textContent = str[0].substring(0, 4) + 'e' + str[1]
+        num = final
+    } else 
+
+    if (`${final}`.length > 8) {
         let str = `${final.toExponential()}`.split('e')
         display.textContent = str[0].substring(0, 4) + 'e' + str[1]
         num = final
@@ -199,3 +225,5 @@ function addAndDisplayNumber(final) {
         num = Number.parseFloat(final.toFixed(2))
     }
 }
+
+
